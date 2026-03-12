30d96be02ab8851020fbaef636130f3ec3ff366a
Anthony Penaloza <apenaloza1@cpp.edu>
2026-03-11 19:36:49 -0700

    Create mod python file with max function, also includes a example dictionary

---
+++ task2_module.py
@@ -0,0 +1,28 @@
+example_dict = {"01":133.5,
+                "02":64.3,
+                "03":104.4,
+                "04":86.3,
+                "05":48.5,
+                "06":35.4,
+                "07":55.3,
+                "08":84.9,
+                "09":104.5,
+                "10":104.4,
+                "11":122.6,
+                "12":119.5}
+
+def maximum_precipitation(dict):
+        max_value = None 
+
+        for value in dict.values(): # Base Case : If dictionary only has one key-value pair
+                max_value = value
+                break
+        
+        for value in dict.values():             # Case 2 : Continues if dictionary has multiple key-value pair(s)
+                if value > max_value:           # Tests current value verus current max
+                        max_value = value       # Updates if current value is greater than current max value
+
+        return max_value                        # Returns max value
+
+max = maximum_precipitation(example_dict)
+print(max)
